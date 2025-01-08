788ce9a414eb4980cec7adc2d43f8bc0b999bd90
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-01-09 01:40:24 +0900

    feat: add question4

---
+++ src/programmers/level1/가장가까운같은글자/index.ts
@@ -0,0 +1,15 @@
+export function solution(s: string) {
+	const arr = s.split("");
+	return arr.map((char, index) => {
+		const prevString = arr.slice(0, index);
+		const prevIndex = prevString.lastIndexOf(char);
+		return prevIndex === -1 ? prevIndex : index - prevIndex;
+	});
+}
+
+export function solution2(s: string) {
+	return [...s].map((char, index) => {
+		const prevIndex = s.substring(0, index).lastIndexOf(char);
+		return prevIndex === -1 ? prevIndex : index - prevIndex;
+	});
+}
