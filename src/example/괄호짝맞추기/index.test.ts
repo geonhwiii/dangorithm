ad04f94195ec132b29b4ab83db11807363c9a214
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-03-02 20:38:22 +0900

    feat: 괄호짝맞추기

---
+++ src/example/괄호짝맞추기/index.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, it } from "vitest";
+import { solution } from "./index";
+
+describe("example_괄호짝맞추기", () => {
+	it("case 1", () => {
+		const s = '(())()'
+		const answer = solution(s);
+		expect(answer).toEqual(true);
+	});
+	it("case 2", () => {
+		const s = '((())()'
+		const answer = solution(s);
+		expect(answer).toEqual(false);
+	});
+});
---
+++ src/example/괄호짝맞추기/index.ts
@@ -0,0 +1,16 @@
+const OPEN = '(';
+
+export function solution(s: string) {
+	const stack = []
+	for (const char of s) {
+		if (char === OPEN) {
+			stack.push(OPEN);
+		} else {
+			if (stack.length === 0) {
+				return false;
+			}
+			stack.pop();
+		}
+	}
+	return stack.length === 0;
+}
