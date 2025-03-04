bf2ddfddebfa2e744d8769d5da84539f3501b363
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-03-04 14:26:11 +0900

    refact: 주식가격 O(n)으로 수정

--- src/programmers/level2/주식가격/index.ts
+++ src/programmers/level2/주식가격/index.ts
@@ -1,8 +1,32 @@
+// O(n)
 export function solution(prices: number[]) {
+	const n = prices.length;
+	const answer = new Array(n).fill(0);
+	const indices = Array.from({ length: n }, (_, i) => i);
+	const remainingStack = indices.reduce((stack, currentIndex) => {
+		const newStack = [];
+		for (const stackIndex of stack) {
+			if (prices[stackIndex] > prices[currentIndex]) {
+				answer[stackIndex] = currentIndex - stackIndex;
+			} else {
+				newStack.push(stackIndex);
+			}
+		}
+		newStack.push(currentIndex);
+		return newStack;
+	}, [] as number[]);
+	for (const index of remainingStack) {
+		answer[index] = n - 1 - index;
+	}
+	return answer;
+}
+// O(n^2)
+function solution2(prices: number[]) {
 	const answer = [];
 	for (let i = 0; i < prices.length; i++) {
 		let count = 0;
 		for (let j = i + 1; j < prices.length; j++) {
+			d;
 			count++;
 			if (prices[i] > prices[j]) {
 				break;
@@ -12,3 +36,7 @@ export function solution(prices: number[]) {
 	}
 	return answer;
 }
+
+// const prices = [1, 2, 3, 2, 3];
+// const answer = solution(prices);
+// expect(answer).toEqual([4, 3, 1, 1, 0]);
