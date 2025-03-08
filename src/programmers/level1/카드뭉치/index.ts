ba2743deee8d68b9d0d1439cc486d15e3de9a543
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-03-08 22:49:33 +0900

    refact: 카드뭉치

--- src/programmers/level1/카드뭉치/index.ts
+++ src/programmers/level1/카드뭉치/index.ts
@@ -1,4 +1,47 @@
+const YES = 'Yes';
+const NO = 'No';
+
+class Queue {
+	list: string[] = [];
+	front = 0;
+	rear = 0;
+
+	constructor(list: string[]) {
+		this.list = list;
+		this.rear = list.length;
+	}
+
+	push(item: string) {
+		this.list.push(item);
+		this.rear++;
+	}
+	pop() {
+		return this.list[this.front++];
+	}
+	size() {
+		return this.rear - this.front;
+	}
+}
+
 export function solution(cards1: string[], cards2: string[], goal: string[]) {
+	const card1Queue = new Queue(cards1);
+	const card2Queue = new Queue(cards2);
+	const goalQueue = new Queue(goal);
+
+	while (goalQueue.size() > 0) {
+		const word = goalQueue.pop();
+		if (word === card1Queue.list[card1Queue.front]) {
+			card1Queue.pop();
+		} else if (word === card2Queue.list[card2Queue.front]) {
+			card2Queue.pop();
+		} else {
+			return NO;
+		}
+	}
+	return YES;
+}
+
+function solution2(cards1: string[], cards2: string[], goal: string[]) {
 	let cards1Index = 0;
 	let cards2Index = 0;
 	for (const word of goal) {
