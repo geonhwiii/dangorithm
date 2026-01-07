3d405540f1abd02c0d216332ffc2344fea9097b6
geonhwiii <geonhwiii@users.noreply.github.com>
2026-01-08 01:40:41 +0900

    feat: plusMinus

---
+++ src/hacker/warm-up/plus-minus/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  let positive = 0;
+  let negative = 0;
+  let zero = 0;
+  const length = arr.length;
+
+  for (const num of arr) {
+    if (num > 0) {
+      positive++;
+    } else if (num < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  }
+
+  console.log((positive / length).toFixed(6));
+  console.log((negative / length).toFixed(6));
+  console.log((zero / length).toFixed(6));
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
