8c254c43b2c8a76ea6a4cc011b16a84138d37e21
geonhwiii <geonhwiii@users.noreply.github.com>
2026-01-08 23:58:12 +0900

    feat: miniMaxSum

---
+++ src/hacker/warm-up/min-max-sum/index.js
@@ -0,0 +1,47 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+  const totalSum = arr.reduce((a, b) => a + b, 0);
+  const minValue = Math.min(...arr);
+  const maxValue = Math.max(...arr);
+
+  const maxSum = totalSum - minValue;
+  const minSum = totalSum - maxValue;
+
+  console.log(minSum, maxSum);
+}
+
+function main() {
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  miniMaxSum(arr);
+}
